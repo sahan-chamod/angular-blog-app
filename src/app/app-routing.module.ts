9c1f4fb8243eb4d6268cc89bc8a089e43401f472
sahan-chamod <sahan7130@gmail.com>
2023-09-29 19:04:40 +0530

    blog-app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
-import { NgModule } from '@angular/core';
+import { NgModule, importProvidersFrom } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './componets/login/login.component';
+import { SignupComponent } from './componets/signup/signup.component';
+import { ForgetPasswordComponent } from './componets/forget-password/forget-password.component';
+import { ResetPasswordComponent } from './componets/reset-password/reset-password.component';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'login' , component:LoginComponent},
+  {path:'signup', component:SignupComponent},
+  {path:'forget-password' , component:ForgetPasswordComponent},
+  {path:'reset-password', component:ResetPasswordComponent},
+  {path:'', component:HomeComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './componets/login/login.component';
+import { SignupComponent } from './componets/signup/signup.component';
+import { ForgetPasswordComponent } from './componets/forget-password/forget-password.component';
+import { ResetPasswordComponent } from './componets/reset-password/reset-password.component';
+import { HeaderComponent } from './layouts/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
+import { PostCardsComponent } from './componets/post-cards/post-cards.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    ForgetPasswordComponent,
+    ResetPasswordComponent,
+    HeaderComponent,
+    HomeComponent,
+    PostCardsComponent
   ],
   imports: [
     BrowserModule,
